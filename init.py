f74d59b9ab4562b1ff6e4d251f053fa3fbf8b6c1
hsuanyu414 <hsuanyu414@gmail.com>
2024-05-05 14:52:32 +0800

    feat: template of all
    1. 新增 gitignore file
    2. 新增初始化檔案(資料庫初始化)
    3. 新增資料庫檔案
    4. 新增 main.py

---
+++ init.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+
+def init_db(db_name='db.sqlite3'):
+
+    """
+    Initialize database with tables
+    
+    user 
+    - user_id (primary key) autoincrement
+    - line_id (unique)
+    - create_date
+
+    record
+    - user_id (foreign key)
+    - record_id (primary key) autoincrement
+    - date
+    - item
+    - cost
+    - category
+    - comment
+    - create_date
+    """
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute('''
+    CREATE TABLE user (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        line_id TEXT UNIQUE,
+        create_date DATE
+    )
+    ''')
+
+    c.execute('''
+    CREATE TABLE record (
+        user_id INTEGER,
+        record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DATE,
+        item TEXT,
+        cost INTEGER,
+        category TEXT,
+        comment TEXT,
+        create_date DATE,
+        FOREIGN KEY(user_id) REFERENCES user(user_id)
+    )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    init_db()
