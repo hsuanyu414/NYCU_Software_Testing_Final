6053937be53adfb94bfe126dbed5c2139c316322
hsuanyu414 <hsuanyu414@gmail.com>
2024-05-09 17:09:04 +0800

    test: refactor test for create_line_user()
    1. 新增回傳值
    2. 新增測試資料
    3. 新增 assert

--- test/line_test.py
+++ test/line_test.py
@@ -4,15 +4,25 @@ import sys
 
 sys.path.append('..')
 from line import line
+from models import User
+import sqlite3
 
 class TestLine:
-    def test_create_line_user(self):
+    @pytest.mark.parametrize("test_line_id, expected", [
+        ('U123456789', 'U123456789'),
+        ('U123456789', 'line_id already exists'),
+        (123456789, 'invalid line_id parameter')
+
+    ])
+    def test_create_line_user(self, test_line_id, expected):
         # Arrange
         module = line.lineFunction()
-        test_line_id = 'U123456789'
-
+        
         # Act
-        ret_val = module.create_line_user(test_line_id)
+        success, user, error_message = module.create_line_user(test_line_id)
 
         # Assert
-        assert ret_val != None
+        if success:
+            assert user.line_id == expected
+        else:
+            assert error_message == expected
