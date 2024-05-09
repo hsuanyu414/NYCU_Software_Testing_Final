1e7ca90f5332b7f83631d1643e824997a48b9706
hsuanyu414 <hsuanyu414@gmail.com>
2024-05-09 17:11:16 +0800

    fix: pass create_line_user()'s test

--- line/line.py
+++ line/line.py
@@ -11,13 +11,27 @@ class lineFunction:
         self.db_name = '../db.sqlite3'
 
     def create_line_user(self, line_id):
+        success = False
+        user = None
+        error_message = None
+
+        # DB related
         conn = sqlite3.connect(self.db_name)
         cursor = conn.cursor()
-        user = None
 
-        if type(line_id) is not str:
-            return None
+        # check if line_id is valid
+        if not isinstance(line_id, str):
+            error_message = 'invalid line_id parameter'
+            return success, user, error_message
 
+        # check if line_id is already in the database
+        cursor.execute('SELECT * FROM user WHERE line_id = ?', (line_id,))
+        row = cursor.fetchone()
+        if row != None:
+            error_message = 'line_id already exists'
+            return success, user, error_message
+
+        
         try:
             # insert new user
             cursor.execute('INSERT INTO user (line_id) VALUES (?)', (line_id,))
@@ -27,10 +41,13 @@ class lineFunction:
             cursor.execute('SELECT * FROM user WHERE user_id = ?', (new_user_id,))
             row = cursor.fetchone()
             user = User.User(user_id=row[0], line_id=row[1], create_date=row[2])
+            success = True
         except Exception as e:
+            error_message = str(e)
             conn.rollback()
 
         conn.commit()
         conn.close()
+        # DB related end
         
-        return user
+        return success, user, error_message
