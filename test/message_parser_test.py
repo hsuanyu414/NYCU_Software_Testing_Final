6b189bcf2f0ab749ce0c903f53b84c4e56d2e58c
bhchen2001 <bhchen312551074.cs12@nycu.edu.tw>
2024-05-15 23:47:00 +0800

    test: mod test case and implement 1. with full parameter 2. test optional parameter 3. test invalid command and pattern

--- test/message_parser_test.py
+++ test/message_parser_test.py
@@ -1,7 +1,11 @@
 import pytest
-import unittest
 
-class MessageParserTest(unittest.TestCase):
+import sys
+sys.path.append('..')
+
+from message_parser import message_parser
+
+class TestMessageParser():
 
     """
     Including following tests:
@@ -10,39 +14,50 @@ class MessageParserTest(unittest.TestCase):
         - test_message_parser_invalid_pattern
     """
 
-    @pytest.mark.parametrize("test_line_id, expected", [
-        ('!記帳 20240101 breakfast 100 food delicious'  , ['create_record', '20240101', 'breakfast', '100', 'food', 'delicious']),
-        ('!最近記帳 最近筆數 10'                         , ['show_recent_record', 'num', 10]),
+    @pytest.mark.parametrize("test_message, expected", [
+        ('!記帳 20240101 breakfast 100 food delicious'  , ['create_record', '20240101', 'breakfast', 100, 'food', 'delicious']),
+        ('!最近記帳 最近筆數 10'                         , ['show_recent_record', 10, 'num']),
         ('!查詢 20240101 20240102'                      , ['search_record', '20240101', '20240102']),
         ('!修改記帳 12345678 日期 20240101 項目 lunch 金額 200 類別 food 備註 delicious'  \
-                                                        , ['update_record', '12345678', 'lunch', '200', 'food', 'delicious']),
+                                                        , ['update_record', '12345678', '20240101', 'lunch', 200, 'food', 'delicious']),
         ('!刪除記帳 12345678'                           , ['delete_record', '12345678']),
         ('!匯出 本月'                                   , ['export_record', 'this_month'])
     ])
-    def test_message_parser_full_info(self, test_line_id, expected):
+    def test_message_parser_full_info(self, test_message, expected):
         """
         Test message with full information
         """
-        pass
+        test_parser = message_parser.MessageParser()
+        success, param_list, error_message = test_parser.parse(test_message)
 
-    @pytest.mark.parametrize("test_line_id, expected", [
-        ('!最近記帳'                                    , ['show_recent_record', 'num']),
+        assert success == True
+        assert param_list == expected
+        assert error_message == None
+
+    @pytest.mark.parametrize("test_message, expected", [
+        ('!最近記帳'                                    , ['show_recent_record']),
         ('!最近記帳 最近筆數'                            , ['show_recent_record', 'num']),
-        ('!最近記帳 最近天數'                            , ['show_recent_record', 'days']),
+        ('!最近記帳 最近天數'                            , ['show_recent_record', 'day']),
+        ('!最近記帳 10'                                 , ['show_recent_record', 10]),
         ('!查詢 20240101'                               , ['search_record', '20240101']),
         ('!匯出'                                        , ['export_record', 'this_month'])
     ])
-    def test_message_parser_optional_info(self):
+    def test_message_parser_optional_info(self, test_message, expected):
         """
         Test message with missing information
             - valid missing information (fill with default value)
             - invalid missing information (raise error)
         """
-        pass
+        test_parser = message_parser.MessageParser()
+        success, param_list, error_message = test_parser.parse(test_message)
 
-    @pytest.mark.parametrize("test_line_id, expected", [
+        assert success == True
+        assert param_list == expected
+        assert error_message == None
+
+    @pytest.mark.parametrize("test_message, expected", [
         ('記帳 20240101 breakfast 100 food delicious'                       , 'invalid command'),
-        ('!繼障 20240101 breakfast 100 food delicious'                      , 'invalid pattern'),
+        ('!繼障 20240101 breakfast 100 food delicious'                      , 'invalid command'),
         ('!記帳 20240101 breakfast not_string food delicious'               , 'invalid pattern'),
         ('!記帳 breakfast 100 food delicious'                               , 'invalid pattern'),
         ('!最近記帳 呵呵'                                                    , 'invalid pattern'),
@@ -54,7 +69,7 @@ class MessageParserTest(unittest.TestCase):
         ('!刪除記帳 12345678 dummy_info'                                     , 'invalid pattern'),
         ('!匯出 本月 dummy_info'                                             , 'invalid pattern'),
     ])
-    def test_message_parser_invalid_pattern(self):
+    def test_message_parser_invalid_pattern(self, test_message, expected):
         """
         Test message with invalid pattern
             - invalid command
@@ -62,4 +77,9 @@ class MessageParserTest(unittest.TestCase):
             - too many parameters
             - invalid parameter type
         """
-        pass
+        test_parser = message_parser.MessageParser()
+        success, param_list, error_message = test_parser.parse(test_message)
+
+        assert success == False
+        assert param_list == []
+        assert error_message == expected
