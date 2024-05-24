04fcc1b820b875509a6ed0e522a28d6382ea0161
bhchen2001 <bhchen312551074.cs12@nycu.edu.tw>
2024-05-24 11:08:13 +0800

    feat: get_user_by_line_id without testing

--- line/line.py
+++ line/line.py
@@ -51,3 +51,37 @@ class lineFunction:
         # DB related end
         
         return success, user, error_message
+    
+    def get_user_by_line_id(self, line_id):
+        '''
+        Get line_id for accounting functions by line_id warpped in MessageEvent object
+        arg: 
+            - line_id (str)
+        '''
+        success = False
+        user = None
+        error_message = None
+
+        # check if line_id is valid
+        if not isinstance(line_id, str):
+            error_message = 'invalid line_id parameter'
+            return success, user, error_message
+        
+        # DB related
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+
+        # find user by line_id
+        cursor.execute('SELECT * FROM user WHERE line_id = ?', (line_id,))
+        row = cursor.fetchone()
+        if row == None:
+            error_message = 'user not found'
+            return success, user, error_message
+        
+        user = User.User(user_id=row[0], line_id=row[1], create_date=row[2])
+
+        success = True
+        conn.close()
+        # DB related end
+
+        return success, user, error_message
